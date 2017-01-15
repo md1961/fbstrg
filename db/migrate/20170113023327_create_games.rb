2deab9fe50ed92ea3f520fa83647cdcde6a00b79
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2017-01-15 12:31:55 +0900

    Add TABLE games to default values

--- db/migrate/20170113023327_create_games.rb
+++ db/migrate/20170113023327_create_games.rb
@@ -2,16 +2,16 @@ class CreateGames < ActiveRecord::Migration
 
   def change
     create_table :games do |t|
-      t.integer :score_home    , default: 0
-      t.integer :score_visitors, default: 0
-      t.integer :timeout_home
-      t.integer :timeout_visitors
-      t.integer :quarter
-      t.integer :time_left
-      t.boolean :is_ball_to_home, null: false
-      t.integer :ball_on
-      t.integer :down
-      t.integer :yard_to_go
+      t.integer :score_home      , default:  0
+      t.integer :score_visitors  , default:  0
+      t.integer :timeout_home    , default:  3
+      t.integer :timeout_visitors, default:  3
+      t.integer :quarter         , default:  1
+      t.integer :time_left       , default: 15 * 60
+      t.boolean :is_ball_to_home , default: true   , null: false
+      t.integer :ball_on         , default: 35
+      t.integer :down            , default:  1
+      t.integer :yard_to_go      , default: 10
 
       t.timestamps null: false
     end
