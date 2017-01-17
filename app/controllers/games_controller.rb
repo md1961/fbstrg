4b9cbfae345f140868b1c86a59b2774b47db1ec9
Naoyuki Kumagai <kumagai@namaka>
2017-01-17 09:14:44 +0900

    Implement simple yardage play preliminarily

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,10 +1,22 @@
 class GamesController < ApplicationController
+  before_action :set_game, only: [:show, :update]
 
   def index
     redirect_to game_path(1)
   end
 
   def show
-    @game = Game.find(params[:id])
   end
+
+  def update
+    @game.play(params[:play])
+    @game.save!
+    render :show
+  end
+
+  private
+
+    def set_game
+      @game = Game.find(params[:id])
+    end
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,15 @@
 class Game < ActiveRecord::Base
+
+  def play(value)
+    if value =~ /\A[-0-9]/
+      value = value.to_i
+      self.ball_on += value
+      self.yard_to_go -= value
+      self.down += 1
+      if yard_to_go <= 0
+        self.down = 1
+        self.yard_to_go = 10
+      end
+    end
+  end
 end
