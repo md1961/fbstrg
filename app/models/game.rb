ad0f58ebaa7c218cea8f18d90929b9e2123e1186
Naoyuki Kumagai <kumagai@namaka>
2017-01-18 08:18:20 +0900

    Game#play() to use Regexp to interpret argument

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,15 +1,19 @@
 class Game < ActiveRecord::Base
 
+  RE_PLAY_VALUE = /\A(?<kind>[a-z]*)(?<yard>-?[0-9]+)/
+
   def play(value)
-    if value =~ /\A[-0-9]/
-      value = value.to_i
-      self.ball_on += value
-      self.yard_to_go -= value
-      self.down += 1
-      if yard_to_go <= 0
-        self.down = 1
-        self.yard_to_go = 10
-      end
+    m = value.match(RE_PLAY_VALUE)
+    return unless m
+    kind = m[:kind]
+    yard = Integer(m[:yard])
+
+    self.ball_on += yard
+    self.yard_to_go -= yard
+    self.down += 1
+    if yard_to_go <= 0
+      self.down = 1
+      self.yard_to_go = 10
     end
   end
 end
