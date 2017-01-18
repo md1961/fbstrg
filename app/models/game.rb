89684e0cedd419170dbbde57d61d12471af77a67
Naoyuki Kumagai <kumagai@namaka>
2017-01-19 08:26:17 +0900

    Game#play() to handle touchdown

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,8 @@
 class Game < ActiveRecord::Base
   attr_accessor :error_message
 
+  KICKOFF_YARDLINE = 35
+
   RE_PLAY_VALUE = /\A(?<kind>[a-z]*)(?<yard>-?[0-9]+)/
 
   def play(value)
@@ -11,18 +13,11 @@ class Game < ActiveRecord::Base
       return
     end
     kind = m[:kind]
+    kind = 'r' if kind.blank?
     yard = Integer(m[:yard])
 
     self.ball_on += yard
-    self.yard_to_go -= yard
-    self.down += 1
-    if yard_to_go <= 0
-      firstdown
-    elsif down > 4
-      self.is_ball_to_home = !is_ball_to_home
-      self.ball_on = 100 - ball_on
-      firstdown
-    end
+    yardage_play(yard)
   end
 
   private
@@ -30,5 +25,31 @@ class Game < ActiveRecord::Base
     def firstdown
       self.down = 1
       self.yard_to_go = 10
+    end
+
+    def touchdown
+      if is_ball_to_home
+        self.score_home += 7
+      else
+        self.score_visitors += 7
+      end
+      firstdown
+      self.ball_on = KICKOFF_YARDLINE
+    end
+
+    def yardage_play(yard)
+      if ball_on >= 100
+        touchdown
+      else
+        self.yard_to_go -= yard
+        self.down += 1
+        if yard_to_go <= 0
+          firstdown
+        elsif down > 4
+          self.is_ball_to_home = !is_ball_to_home
+          self.ball_on = 100 - ball_on
+          firstdown
+        end
+      end
     end
 end
