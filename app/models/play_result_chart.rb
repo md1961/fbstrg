ae40959ceac7f8940de3a4d7a85767c079cee63d
Naoyuki Kumagai <kumagai@namaka>
2017-01-24 15:48:29 +0900

    Add PlayResultChart#result()

--- app/models/play_result_chart.rb
+++ app/models/play_result_chart.rb
@@ -1,3 +1,7 @@
 class PlayResultChart < ActiveRecord::Base
   has_many :play_results
+
+  def result(offensive_play, defensive_play)
+    play_results.find_by(offensive_play: offensive_play, defensive_play: defensive_play).result
+  end
 end
