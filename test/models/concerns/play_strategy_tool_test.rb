5531b649a350d69cca0ad62bc56d487077e6c436
Naoyuki Kumagai <kumagai@namaka>
2017-01-24 15:15:05 +0900

    Create preliminary test for PlayStrategyTool#pick_from()

---
+++ test/models/concerns/play_strategy_tool_test.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+class PlayStrategyToolTest < ActiveSupport::TestCase
+  include PlayStrategyTool
+
+  test "#pick_from()" do
+    weightables = 10.times.map { |id| Weightable.new(id, 100) }
+    h_freq = 10000.times.inject(Hash.new { |h, k| h[k] = 0 }) do |h, i|
+      h[pick_from(weightables).id] += 1
+      h
+    end
+    10.times do |i|
+      assert_in_delta(1000, h_freq[i], 100)
+    end
+  end
+
+  class Weightable
+    attr_accessor :id, :weight
+
+    def initialize(id, weight)
+      @id     = id
+      @weight = weight
+    end
+  end
+end
