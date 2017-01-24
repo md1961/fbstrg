bde5e626bcb5aca33be2943650587f347d1d8e6b
Naoyuki Kumagai <kumagai@namaka>
2017-01-24 12:20:17 +0900

    Add OffensivePlayStrategy#choose()

--- app/models/offensive_play_strategy.rb
+++ app/models/offensive_play_strategy.rb
@@ -1,3 +1,15 @@
 class OffensivePlayStrategy < ActiveRecord::Base
   has_many :offensive_play_strategy_weights
+
+  def choose
+    obj_weights = offensive_play_strategy_weights
+    total_weights = obj_weights.map(&:weight).sum
+    pick = rand(total_weights)
+    obj_weight_picked = obj_weights.inject(0) do |cum_weight, obj_weight|
+      cum_weight += obj_weight.weight
+      break obj_weight if pick < cum_weight
+      cum_weight
+    end
+    obj_weight_picked.offensive_play
+  end
 end
