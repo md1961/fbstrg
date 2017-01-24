77a2bd778ac92942876960f902e0a48d490f44e9
Naoyuki Kumagai <kumagai@namaka>
2017-01-24 14:01:26 +0900

    Add DefensivePlayStrategy#choose()

--- app/models/defensive_play_strategy.rb
+++ app/models/defensive_play_strategy.rb
@@ -1,3 +1,9 @@
 class DefensivePlayStrategy < ActiveRecord::Base
+  include PlayStrategyTool
+
   has_many :defensive_play_strategy_weights
+
+  def choose
+    pick_from(defensive_play_strategy_weights).defensive_play
+  end
 end
