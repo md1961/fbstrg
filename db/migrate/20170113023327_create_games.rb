ff0a12059dc92a6c59d9c21342ea2b04649576e0
Naoyuki Kumagai <kumagai@namaka>
2017-01-25 07:54:22 +0900

    Add COLUMN home_id and visitors_id to Game

--- db/migrate/20170113023327_create_games.rb
+++ db/migrate/20170113023327_create_games.rb
@@ -2,16 +2,18 @@ class CreateGames < ActiveRecord::Migration
 
   def change
     create_table :games do |t|
-      t.integer :score_home      , default:  0
-      t.integer :score_visitors  , default:  0
-      t.integer :timeout_home    , default:  3
-      t.integer :timeout_visitors, default:  3
-      t.integer :quarter         , default:  1
-      t.integer :time_left       , default: 15 * 60
+      t.integer :home_id         , default:  1     , null: false
+      t.integer :visitors_id     , default:  1     , null: false
+      t.integer :score_home      , default:  0     , null: false
+      t.integer :score_visitors  , default:  0     , null: false
+      t.integer :timeout_home    , default:  3     , null: false
+      t.integer :timeout_visitors, default:  3     , null: false
+      t.integer :quarter         , default:  1     , null: false
+      t.integer :time_left       , default: 15 * 60, null: false
       t.boolean :is_ball_to_home , default: true   , null: false
-      t.integer :ball_on         , default: 35
-      t.integer :down            , default:  1
-      t.integer :yard_to_go      , default: 10
+      t.integer :ball_on         , default: 35     , null: false
+      t.integer :down            , default:  1     , null: false
+      t.integer :yard_to_go      , default: 10     , null: false
 
       t.timestamps null: false
     end
