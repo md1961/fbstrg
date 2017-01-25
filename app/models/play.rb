5c1943215c20db6ff803e25767d952fd15f15d62
Naoyuki Kumagai <kumagai@namaka>
2017-01-25 14:21:57 +0900

    Rename enum keys to self-descriptive ones in Play

--- app/models/play.rb
+++ app/models/play.rb
@@ -1,5 +1,5 @@
 class Play < ActiveRecord::Base
   enum result:  {on_ground: 0, complete: 1, incomplete: 2, intercepted: 3, sacked: 4}
-  enum fumble:  {no_fumble: 0, by_own: 1, by_opponent: 2}
-  enum penalty: {no_penalty: 0, by_offense: 1, by_defense: 2}
+  enum fumble:  {no_fumble: 0, fumble_rec_by_own: 1, fumble_rec_by_opponent: 2}
+  enum penalty: {no_penalty: 0, off_penalty: 1, def_penalty: 2}
 end
