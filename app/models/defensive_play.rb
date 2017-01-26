6b3a6571f6c63ec6a68f2152fa0c4d8c41fa8a0d
Naoyuki Kumagai <kumagai@namaka>
2017-01-26 10:32:18 +0900

    Add OffensivePlay#to_s() and DefensivePlay#to_s()

--- app/models/defensive_play.rb
+++ app/models/defensive_play.rb
@@ -1,3 +1,7 @@
 class DefensivePlay < ActiveRecord::Base
   has_many :play_results
+
+  def to_s
+    "#{name}: #{[lineman, linebacker, cornerback, safety].join(',')}\nRun: #{against_run}, Pass: #{against_pass}"
+  end
 end
--- app/models/offensive_play.rb
+++ app/models/offensive_play.rb
@@ -1,3 +1,7 @@
 class OffensivePlay < ActiveRecord::Base
   has_many :play_results
+
+  def to_s
+    name
+  end
 end
