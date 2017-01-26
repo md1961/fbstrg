fa7a3f9d86dad94a1e0dc572a14eac5102ff93ea
Naoyuki Kumagai <kumagai@namaka>
2017-01-26 13:30:26 +0900

    Modify DefensivePlay#to_s() to use - as delimiter in formation string

--- app/models/defensive_play.rb
+++ app/models/defensive_play.rb
@@ -2,6 +2,8 @@ class DefensivePlay < ActiveRecord::Base
   has_many :play_results
 
   def to_s
-    "#{name}: #{[lineman, linebacker, cornerback, safety].join(',')}\nRun: #{against_run}, Pass: #{against_pass}"
+    formation = [lineman, linebacker, cornerback, safety].join('-')
+    "#{name}: #{formation}\n" \
+      + "Run: #{against_run}, Pass: #{against_pass}"
   end
 end
