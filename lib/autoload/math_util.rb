37f3ae5f70a99bcb65dbd44fd58199545fdeb762
Naoyuki Kumagai <kumagai@namaka>
2017-01-27 09:01:37 +0900

    Add MathUtil.linear_interporation()

---
+++ lib/autoload/math_util.rb
@@ -0,0 +1,9 @@
+module MathUtil
+  module_function
+
+  def linear_interporation(coord1, coord2, x)
+    x1, y1 = coord1
+    x2, y2 = coord2
+    (y1 + 1.0 * (y2 - y1) / (x2 - x1) * (x - x1)).round
+  end
+end
