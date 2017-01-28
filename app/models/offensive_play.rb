e3d0df47a5081ac8347acf3794b75720944c931e
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2017-01-28 15:30:14 +0900

    Add OffensivePlay.punt(), .field_goal(), #punt?() and #field_goal?()

--- app/models/offensive_play.rb
+++ app/models/offensive_play.rb
@@ -1,6 +1,22 @@
 class OffensivePlay < ActiveRecord::Base
   has_many :play_results
 
+  def self.punt
+    @punt ||= find_by(name: 'Punt')
+  end
+
+  def self.field_goal
+    @field_goal ||= find_by(name: 'Field Goal')
+  end
+
+  def punt?
+    name == 'Punt'
+  end
+
+  def field_goal?
+    name == 'Field Goal'
+  end
+
   def to_s
     name
   end
