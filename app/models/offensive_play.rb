67c7686e9fab6240e26a815868d8e0c7a3536388
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2017-01-28 17:46:28 +0900

    Add OffensivePlay.kickoff(), .extra_point(), #kickoff?(), #extra_point?()

--- app/models/offensive_play.rb
+++ app/models/offensive_play.rb
@@ -1,6 +1,12 @@
 class OffensivePlay < ActiveRecord::Base
   has_many :play_results
 
+  # TODO: Define all by one statement.
+
+  def self.kickoff
+    @punt ||= find_by(name: 'Kickoff')
+  end
+
   def self.punt
     @punt ||= find_by(name: 'Punt')
   end
@@ -9,12 +15,24 @@ class OffensivePlay < ActiveRecord::Base
     @field_goal ||= find_by(name: 'Field Goal')
   end
 
+  def self.extra_point
+    @field_goal ||= find_by(name: 'Extra Point')
+  end
+
+  def kickoff?
+    name == 'Kickoff'
+  end
+
   def punt?
     name == 'Punt'
   end
 
   def field_goal?
     name == 'Field Goal'
+  end
+
+  def extra_point?
+    name == 'Extra Point'
   end
 
   def to_s
