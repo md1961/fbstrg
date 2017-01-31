bbf9dcb5fbf3d3531ed809ab8c8d1c9d3bd350fd
Naoyuki Kumagai <kumagai@namaka>
2017-01-31 11:47:46 +0900

    Remove unused Team#offensive_play_set() and #defensive_play_set()

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,14 +3,6 @@ class Team < ActiveRecord::Base
   belongs_to :offensive_strategy
   belongs_to :defensive_strategy
 
-  def offensive_play_set
-    @offensive_play_set ||= OffensivePlaySet.find_by(name: 'Standard')
-  end
-
-  def defensive_play_set
-    @defensive_play_set ||= DefensivePlaySet.find_by(name: 'Standard')
-  end
-
   def to_s
     name
   end
