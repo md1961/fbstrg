05b7604c9958e59bb7899005b7f40036c0664276
Naoyuki Kumagai <kumagai@namaka>
2017-02-01 08:28:40 +0900

    Modify DefensiveStrategy to choose DefensivePlaySet using StrategyTool

--- app/models/defensive_strategy.rb
+++ app/models/defensive_strategy.rb
@@ -1,10 +1,18 @@
 class DefensiveStrategy < ActiveRecord::Base
+  include StrategyTool
 
-  def defensive_play_set
-    @defensive_play_set ||= DefensivePlaySet.find_by(name: 'Standard')
+  def defensive_play_set(game)
+    if defense_running_out_of_time?(game)
+      DefensivePlaySet.expect_run
+    elsif offense_running_out_of_time?(game)
+      DefensivePlaySet.prevent
+    else
+      DefensivePlaySet.standard
+    end
   end
 
   def choose_play(game)
-    defensive_play_set.choose
+    play_set = defensive_play_set(game)
+    play_set.choose
   end
 end
