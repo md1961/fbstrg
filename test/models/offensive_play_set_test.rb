90b8f7e78e0acf402a1c4c885da9bfe634725a52
Naoyuki Kumagai <kumagai@namaka>
2017-02-02 08:03:10 +0900

    Modify OffensivePlaySetTest to test play choice limitation

--- test/models/offensive_play_set_test.rb
+++ test/models/offensive_play_set_test.rb
@@ -1,7 +1,39 @@
 require 'test_helper'
 
 class OffensivePlaySetTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    @play_set = offensive_play_sets(:standard)
+    @game = games(:one)
+  end
+
+  def count_freq(total_counts)
+    total_counts.times.inject(Hash.new { |h, k| h[k] = 0 }) do |h, i|
+      play = @play_set.choose(@game)
+      h[play.number] += 1
+      h
+    end
+  end
+
+  test '#choose for play choice limitation' do
+    @game.ball_on = 79
+    h_freq = count_freq(1000)
+    assert_equal((1 .. 20).to_a, h_freq.keys.map(&:to_i).sort)
+
+    @game.ball_on = 80
+    h_freq = count_freq(1000)
+    assert_equal((1 .. 16).to_a, h_freq.keys.map(&:to_i).sort)
+
+    @game.ball_on = 89
+    h_freq = count_freq(1000)
+    assert_equal((1 .. 16).to_a, h_freq.keys.map(&:to_i).sort)
+
+    @game.ball_on = 90
+    h_freq = count_freq(1000)
+    assert_equal((1 .. 12).to_a, h_freq.keys.map(&:to_i).sort)
+
+    @game.ball_on = 99
+    h_freq = count_freq(1000)
+    assert_equal((1 .. 12).to_a, h_freq.keys.map(&:to_i).sort)
+  end
 end
