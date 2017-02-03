e95bc432e82c1bde91e5a9a428ad3bce30c2bcf9
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2017-02-04 06:08:39 +0900

    Fix enum status of Game with default of :huddle for opening kickoff

--- app/models/game.rb
+++ app/models/game.rb
@@ -8,7 +8,7 @@ class Game < ActiveRecord::Base
   attr_accessor :offensive_play, :error_message
 
   enum next_play: {kickoff: 0, extra_point: 1, two_point_conversion: 2, scrimmage: 3}
-  enum status: {playing: 0, huddle: 1, end_of_quarter: 2, end_of_half: 3, end_of_game: 4}
+  enum status: {huddle: 0, playing: 1, end_of_quarter: 2, end_of_half: 3, end_of_game: 4}
 
   KICKOFF_YARDLINE = 35
   TOUCHBACK_YARDLINE = 20
