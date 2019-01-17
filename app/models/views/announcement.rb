617b8f7ed41dd3f6caeb90b62702396cd851c5d0
Naoyuki Kumagai <kumagai@namaka>
2019-01-17 11:05:58 +0900

    Add model Views::Announcement

---
+++ app/models/views/announcement.rb
@@ -0,0 +1,36 @@
+module Views
+
+class Announcement
+
+  def initialize
+    @statements = []
+  end
+
+  def add(text, timeout)
+    @statements << Statement.new(text, timeout)
+    self
+  end
+
+  def last_text
+    @statements.last&.text
+  end
+
+  def to_s
+    "[#{@statements.join(',')}]"
+  end
+
+  class Statement
+    attr_reader :text, :timeout
+
+    def initialize(text, timeout)
+      @text = text
+      @timeout = timeout
+    end
+
+    def to_s
+      %Q!["#{text}",#{timeout}]!
+    end
+  end
+end
+
+end
