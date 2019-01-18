981ced3ac77965cb85cc57223d078359d6cb7c82
Naoyuki Kumagai <kumagai@namaka>
2019-01-18 09:27:52 +0900

    Correct helper offensive_play_display() to use Game#defense_human?() to judge hiding

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -34,7 +34,7 @@ module GamesHelper
   end
 
   def offensive_play_display(game)
-    return 'X' * 10 if !game.offense_human? && game.playing?
+    return 'X' * 10 if game.defense_human? && game.playing?
     play_set = game.offensive_play_set
     "#{game.offensive_play}#{play_set.blank? ? '' : " : from #{play_set}"}"
   end
