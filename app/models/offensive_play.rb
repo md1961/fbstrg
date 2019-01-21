178bb0390dd6797cb3ee71dafc64ed7b80376178
Naoyuki Kumagai <kumagai@namaka>
2019-01-21 13:28:47 +0900

    Alter OffensivePlay to auto-define methods such as .sweep, #sweep?, etc.

--- app/models/offensive_play.rb
+++ app/models/offensive_play.rb
@@ -1,11 +1,10 @@
 class OffensivePlay < ActiveRecord::Base
   has_many :play_results
 
-  NAMES_FOR_AUTO_DEF = OffensivePlay.where('number > 100').pluck(:name)
-
+  # Define methods such as OffensivePlay.power_up_middle, etc.
   class << self
-    NAMES_FOR_AUTO_DEF.each do |name|
-      method_name = name.titleize.gsub(/\s+/, '').underscore
+    OffensivePlay.pluck(:name).each do |name|
+      method_name = name.titleize.gsub(/[\s&]+/, '').underscore
       define_method method_name do
         instance_variable_get(:"@#{method_name}") \
           || instance_variable_set(:"@#{method_name}", find_by(name: name))
@@ -13,8 +12,9 @@ class OffensivePlay < ActiveRecord::Base
     end
   end
 
-  NAMES_FOR_AUTO_DEF.each do |name|
-    method_name = name.titleize.gsub(/\s+/, '').underscore
+  # Define methods such as OffensivePlay#power_up_middle?, etc.
+  pluck(:name).each do |name|
+    method_name = name.titleize.gsub(/[\s&]+/, '').underscore
     define_method "#{method_name}?" do
       self.name == name
     end
@@ -30,18 +30,6 @@ class OffensivePlay < ActiveRecord::Base
 
   def inside_20?
     number <= 16
-  end
-
-  def quarterback_keep?
-    number == 3
-  end
-
-  def sweep?
-    number == 5
-  end
-
-  def draw?
-    number == 7
   end
 
   def kickoff?
