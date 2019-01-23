4a2c1affab101e088319e0182338fd6472993693
Naoyuki Kumagai <kumagai@namaka>
2019-01-23 13:48:07 +0900

    Fix service Clocker not to stop clock when :fumble_rec_by_own

--- app/services/clocker.rb
+++ app/services/clocker.rb
@@ -3,8 +3,8 @@ module Clocker
 
   def time_to_take(play, game)
     play.time_to_take = 0 if play.extra_point?
-    game.clock_stopped = play.incomplete? || play.intercepted? || play.fumble? ||
-      play.out_of_bounds? || play.field_goal? || play.kick_and_return? || play.penalty?
+    game.clock_stopped = play.incomplete? || play.intercepted? || play.fumble_rec_by_opponent? ||
+                  play.out_of_bounds? || play.field_goal? || play.kick_and_return? || play.penalty?
     play.time_to_take
   end
 end
