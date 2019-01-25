d8f287aef3a35e84bd8a12c4f752e1eb09ebb63b
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2019-01-25 15:51:22 +0900

    Modify 'off_def_charts#index' to display PlayChartResult on default

--- app/controllers/off_def_charts_controller.rb
+++ app/controllers/off_def_charts_controller.rb
@@ -1,8 +1,14 @@
 class OffDefChartsController < ApplicationController
 
   def index
+    @type = params[:type]
     @f_item = ->(offensive_play, defensive_play) {
-      "%5.1f" % Play.pct_intercept_base(offensive_play, defensive_play)
+      case @type
+      when 'int'
+        "%5.1f" % Play.pct_intercept_base(offensive_play, defensive_play)
+      else
+        PlayResultChart.first.result(offensive_play, defensive_play)
+      end
     }
     @offensive_plays = OffensivePlay.pass_plays
     @defensive_plays = DefensivePlay.all
