3cc9397b0f568b9aca10c5ae696082ed92eabfca
Naoyuki Kumagai <kumagai@namaka>
2019-01-30 11:49:19 +0900

    Modify StrategyTool for hurry-up situations

--- app/models/concerns/strategy_tool.rb
+++ app/models/concerns/strategy_tool.rb
@@ -5,6 +5,8 @@ module StrategyTool
   VERY_SHORT_YARDAGE = 2
   MINUTES_ENDING_HALF = 4
 
+  MINUTES_TO_MOVE_5_YARDS = 40.0 / 60 / 8 * 5
+
   def offense_running_out_of_time?(game)
     game.score_diff < 0 && time_running_out?(game)
   end
@@ -13,9 +15,16 @@ module StrategyTool
     game.score_diff > 0 && time_running_out?(game)
   end
 
+  # TODO: Add conditions for possible-tie or sudden-death in overtime.
   def time_running_out?(game)
-    game.quarter >= 4 && \
-      game.time_left / (game.score_diff.abs.to_f / 7) < MINUTES_NEEDED_FOR_TOUCHDOWN * 60
+    game.quarter == 4 && (
+      (game.score_diff <= -3 && game.time_left <= minutes_needed_for_touchdown(game)) ||
+      (game.score_diff <=  0 && game.time_left <= minutes_needed_for_field_goal(game))
+    )
+  end
+
+  def need_to_hurry_before_halftime?(game)
+    game.quarter == 2 && game.score_diff <= 7 && game.time_left <= minutes_needed_for_touchdown(game)
   end
 
   def half_ending?(game)
@@ -37,4 +46,14 @@ module StrategyTool
   def need_very_short_yardage?(game)
     game.down == 3 && game.yard_to_go <= VERY_SHORT_YARDAGE
   end
+
+    def minutes_needed_for_touchdown(game)
+      (100 - game.ball_on) / 5.0 * MINUTES_TO_MOVE_5_YARDS
+    end
+
+    YARD_LINE_TO_REACH_FOR_FIELD_GOAL = 25
+
+    def minutes_needed_for_field_goal(game)
+      (100 - YARD_LINE_TO_REACH_FOR_FIELD_GOAL - game.ball_on) / 5.0 * MINUTES_TO_MOVE_5_YARDS
+    end
 end
