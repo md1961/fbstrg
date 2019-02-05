01aee3dd19b372b9171b421a9212d15e600157d1
Naoyuki Kumagai <kumagai@namaka>
2019-02-06 08:17:55 +0900

    Add :kickoff_after_safety to enum :next_play of GameEnum

--- app/models/concerns/game_enum.rb
+++ app/models/concerns/game_enum.rb
@@ -1,7 +1,7 @@
 module GameEnum
 
   def self.extended(obj)
-    obj.enum next_play: {kickoff: 0, extra_point: 1, two_point_conversion: 2, scrimmage: 3}
+    obj.enum next_play: {kickoff: 0, extra_point: 1, two_point_conversion: 2, scrimmage: 3, kickoff_after_safety: 4}
     obj.enum status: {huddle: 0, playing: 1, end_of_quarter: 2, end_of_half: 3, end_of_game: 4}
   end
 end
