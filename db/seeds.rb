d3b4b0e2defe09fb4c08bfbb983fdb18431b9624
Naoyuki Kumagai <kumagai@namaka>
2019-02-07 14:44:15 +0900

    Start on addition of TeamTrait's in db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -224,13 +224,39 @@ home_team = Team.create!(
   play_result_chart:  result_chart,
   offensive_strategy: offensive_strategy,
   defensive_strategy: defensive_strategy,
-)
+).tap { |t| t.create_team_trait! }
 visitors  = Team.create!(
   name:               'Visitors',
   abbr:               'V',
   play_result_chart:  result_chart,
   offensive_strategy: offensive_strategy,
   defensive_strategy: defensive_strategy,
-)
+).tap { |t| t.create_team_trait! }
 
 Game.create!(home_team: home_team, visitors: visitors)
+
+Team.create!(
+  name:               'Miami',
+  abbr:               'MIA',
+  play_result_chart:  result_chart,
+  offensive_strategy: offensive_strategy,
+  defensive_strategy: defensive_strategy,
+).create_team_trait!(
+  run_yardage:      -3,
+  run_breakaway:    -3,
+  pass_short:        5,
+  pass_long:         5,
+  pass_breakaway:    5,
+  pass_protect:      5,
+  qb_mobility:      -5,
+
+  run_defense:       0,
+  run_tackling:      0,
+  pass_rush:         3,
+  pass_coverage:     0,
+  pass_tackling:     0,
+
+  place_kicking:     4,
+  return_breakaway:  0,
+  return_coverage:   0,
+)
