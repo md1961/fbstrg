51e3ffae1431b9fa7e482b16c0281b0e97b78a41
Naoyuki Kumagai <kumagai@namaka>
2019-02-08 11:14:24 +0900

    Add PlayResultChart#min_yardage() and #max_yardage()

--- app/models/play_result_chart.rb
+++ app/models/play_result_chart.rb
@@ -5,10 +5,32 @@ class PlayResultChart < ApplicationRecord
 
   def init
     @results = {}
+    @min_and_max_yardages = {}
   end
 
   def result(offensive_play, defensive_play)
     @results[[offensive_play.id, defensive_play.id]] ||= \
       play_results.find_by(offensive_play: offensive_play, defensive_play: defensive_play).result
   end
+
+  def min_yardage(offensive_play)
+    min_and_max_yardages(offensive_play)[0]
+  end
+
+  def max_yardage(offensive_play)
+    min_and_max_yardages(offensive_play)[1]
+  end
+
+  private
+
+    def min_and_max_yardages(offensive_play)
+      @min_and_max_yardages[offensive_play.id] ||= \
+        DefensivePlay.all.each_with_object([999, -999]) { |defensive_play, min_and_max|
+          r = result(offensive_play, defensive_play)
+          next if r !~ /[+-]?\d+/
+          yardage = r.sub(/[^+-]*([+-]?\d+).*/, '\1').to_i
+          min_and_max[0] = yardage if yardage < min_and_max[0]
+          min_and_max[1] = yardage if yardage > min_and_max[1]
+        }
+    end
 end
