1bce7be3a53b6c37f4163b41fa6c782c72cb3928
Naoyuki Kumagai <kumagai@namaka>
2019-02-08 11:13:48 +0900

    Modify PlayResultChart#result() to cache return values

--- app/models/play_result_chart.rb
+++ app/models/play_result_chart.rb
@@ -1,7 +1,14 @@
 class PlayResultChart < ApplicationRecord
   has_many :play_results, dependent: :destroy
 
+  after_find :init
+
+  def init
+    @results = {}
+  end
+
   def result(offensive_play, defensive_play)
-    play_results.find_by(offensive_play: offensive_play, defensive_play: defensive_play).result
+    @results[[offensive_play.id, defensive_play.id]] ||= \
+      play_results.find_by(offensive_play: offensive_play, defensive_play: defensive_play).result
   end
 end
