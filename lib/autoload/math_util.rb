479c23e6f2400dc1d8089713621d4be193a931af
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2019-02-10 14:34:45 +0900

    Add MathUtil.pick_from_decreasing_distribution()

--- lib/autoload/math_util.rb
+++ lib/autoload/math_util.rb
@@ -6,4 +6,13 @@ module MathUtil
     x2, y2 = coord2
     (y1 + 1.0 * (y2 - y1) / (x2 - x1) * (x - x1))
   end
+
+  def pick_from_decreasing_distribution(min, max)
+    min, max = max, min if min > max
+    offset = max - min + 1
+    while true
+      x = rand(offset) - rand(offset)
+      return min + x if x >= 0
+    end
+  end
 end
