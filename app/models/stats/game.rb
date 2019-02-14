66a31febda6e8fc6804508bf3b968f8dbd881086
Naoyuki Kumagai <kumagai@namaka>
2019-02-14 11:35:57 +0900

    Rename Game#stats_home_team to #stats_home

--- app/models/stats/game.rb
+++ app/models/stats/game.rb
@@ -1,11 +1,11 @@
 module Stats
 
 class Game
-  attr_reader :stats_home_team, :stats_visitors
+  attr_reader :stats_home, :stats_visitors
 
   def initialize(game)
     @game = game
-    @stats_home_team = Stats::Team.new(game.home_team)
+    @stats_home      = Stats::Team.new(game.home_team)
     @stats_visitors  = Stats::Team.new(game.visitors )
     tally
   end
@@ -16,7 +16,7 @@ class Game
       plays = @game.plays.includes(:game_snapshot)
       PlayUtil.write_scorings(plays)
       plays.each do |play|
-        stat = play.game_snapshot.home_has_ball ? @stats_home_team : @stats_visitors
+        stat = play.game_snapshot.home_has_ball ? @stats_home : @stats_visitors
         stat.tally_from(play)
       end
     end
