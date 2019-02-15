1a8a1e9aa82348f7edde20ba725206a4238402d3
Naoyuki Kumagai <kumagai@namaka>
2019-02-15 11:29:18 +0900

    Alter kneel_down_to_finish_half?() a little more likely to return false

--- app/models/concerns/strategy_tool.rb
+++ app/models/concerns/strategy_tool.rb
@@ -132,7 +132,7 @@ module StrategyTool
 
   def kneel_down_to_finish_half?(game)
     game.quarter == 2 && game.score_diff >= -14 && game.ball_on > 2 && game.ball_on <= 30 \
-      && game.time_left <= 40 * (5 - game.down - (game.clock_stopped? ? 1 : 0)) - 39 * game.timeout_left(false)
+      && game.time_left <= 30 * (5 - game.down - (game.clock_stopped? ? 1 : 0) - game.timeout_left(false))
   end
 
     def zone_conservative?(game)
