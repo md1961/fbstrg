56b3ba1bc91961eeeb2831ade445506c1f006625
Naoyuki Kumagai <kumagai@namaka>
2019-02-15 13:41:53 +0900

    Modify tries_fourth_down_gamble?() to avoid gamble from conservative zone

--- app/models/concerns/strategy_tool.rb
+++ app/models/concerns/strategy_tool.rb
@@ -70,6 +70,8 @@ module StrategyTool
   def tries_fourth_down_gamble?(game)
     return false unless game.down == 4
     return false if game.quarter == 4 && game.score_diff > 0
+    return false if game.quarter == 4 && game.score_diff == 0 && zone_conservative?(game)
+    return false if game.quarter == 4 && game.score_diff <  0 && zone_conservative?(game) && game.time_left >= 4 * 60
     return false if game.quarter == 2 && game.time_left <= seconds_needed_for_field_goal(game)
     return false if game.quarter <= 2 && game.score_diff >= 0 && game.ball_on < 55 + rand(6)
     game.quarter == 4 && (
