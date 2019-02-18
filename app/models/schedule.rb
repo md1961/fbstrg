24667def57aabd0ba3db107869611ea7ecbdec32
Naoyuki Kumagai <kumagai@namaka>
2019-02-18 11:57:38 +0900

    Add Schedule#for?(team)

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -2,6 +2,10 @@ class Schedule < ApplicationRecord
   belongs_to :team_group
   belongs_to :game
 
+  def for?(team)
+    game.for?(team)
+  end
+
   def to_s
     "#{team_group} week #{week}: #{game}"
   end
