336ea7ded391db4217b054a8f9e98f04b75337b1
Naoyuki Kumagai <kumagai@namaka>
2019-02-18 10:52:18 +0900

    Modify Team to add dependent: :destroy to has_one :team_trait

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,7 +2,7 @@ class Team < ApplicationRecord
   belongs_to :play_result_chart
   belongs_to :offensive_strategy
   belongs_to :defensive_strategy
-  has_one :team_trait
+  has_one :team_trait, dependent: :destroy
 
   def to_s
     name
