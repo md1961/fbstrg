55aad52a2cf5b09ee05a26588c401157e0cdb742
Naoyuki Kumagai <kumagai@namaka>
2019-02-18 14:33:40 +0900

    Correct tries_fourth_down_gamble?() to take yard-to-go into account

--- app/models/concerns/strategy_tool.rb
+++ app/models/concerns/strategy_tool.rb
@@ -81,7 +81,9 @@ module StrategyTool
     return false if game.quarter == 4 && game.score_diff > 0
     return false if game.quarter == 4 && game.score_diff == 0 && zone_conservative?(game)
     return false if game.quarter == 4 && game.score_diff <  0 && zone_conservative?(game) && game.time_left >= 4 * 60
+    return false if game.quarter == 4 && game.score_diff >= -14 && game.ball_on < 40 && game.yard_to_go > 10 && game.time_left >= 10 * 60
     return false if game.quarter == 3 && game.score_diff > 14
+    return false if game.quarter <= 3 && game.yard_to_go > 10
     return false if game.quarter == 2 && game.time_left <= seconds_needed_for_field_goal(game)
     return false if game.quarter <= 2 && game.score_diff >= 0 && game.ball_on < 55 + rand(6)
     game.quarter == 4 && (
