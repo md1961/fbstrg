3b7e7a855cc9c40628f7009b0f620b35132135b5
Naoyuki Kumagai <kumagai@namaka>
2019-02-18 13:59:55 +0900

    Modify 'leagues#show' to display game results

---
+++ app/helpers/leagues_helper.rb
@@ -0,0 +1,10 @@
+module LeaguesHelper
+
+  def game_result_display(game, team)
+    return nil unless game&.end_of_game? && game&.for?(team)
+    score_own, score_opp = game.score_home, game.score_visitors
+    score_own, score_opp = score_opp, score_own if team == game.visitors
+    r = %w[L T W][(score_own <=> score_opp) + 1]
+    "#{r} #{score_own} - #{score_opp}"
+  end
+end
