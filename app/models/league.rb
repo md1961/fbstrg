4d3cecbfc82c2e501d69278cbf2e637e3b845b78
Naoyuki Kumagai <kumagai@namaka>
2019-02-18 10:50:57 +0900

    Fix League#make_schedules() to play home-and-away with each others

--- app/models/league.rb
+++ app/models/league.rb
@@ -2,14 +2,17 @@ class League < TeamGroup
   has_many :schedules, foreign_key: 'team_group_id', dependent: :destroy
 
   def make_schedules
+    return if teams.empty?
     team_ids = teams.map(&:id).sort_by { rand }
     raise "Not implemented yet for even number of Team's" if team_ids.size.even?
-    (team_ids.size - 1).times do |week|
+    team_ids.size.times do |week|
       (team_ids.size / 2).times do |i|
         h = Team.find(team_ids[i])
         v = Team.find(team_ids[-(i + 1)])
         game = Game.new(home_team: h, visitors: v)
         schedules.build(week: week + 1, number: i + 1, game: game)
+        game = Game.new(home_team: v, visitors: h)
+        schedules.build(week: week + 1 + team_ids.size, number: i + 1, game: game)
       end
       team_ids.unshift(team_ids.pop)
     end
