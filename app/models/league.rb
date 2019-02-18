842b47382653f1b5f49a0f57e7e13ed4d4d6eb26
Naoyuki Kumagai <kumagai@namaka>
2019-02-18 11:08:45 +0900

    Fix League#next_game() by renaming to #next_schedule()

--- app/models/league.rb
+++ app/models/league.rb
@@ -2,7 +2,7 @@ class League < TeamGroup
   has_many :schedules, -> { order(:week, :number) },
                        foreign_key: 'team_group_id', dependent: :destroy
 
-  def next_game
+  def next_schedule
     return nil if schedules.empty?
     schedules.detect { |schedule| !schedule.game.end_of_game? }
   end
