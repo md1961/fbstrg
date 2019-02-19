4e2cd23ed7af394908b7747a71f4b96a6dbd5377
Naoyuki Kumagai <kumagai@namaka>
2019-02-19 09:32:45 +0900

    Modify 'leagues#show' to display team W-L-T record

--- app/helpers/leagues_helper.rb
+++ app/helpers/leagues_helper.rb
@@ -1,5 +1,11 @@
 module LeaguesHelper
 
+  def team_result_display_for(team, league)
+    results = league.game_results_for(team)
+    results.pop if results.last.zero?
+    results.join('-')
+  end
+
   def game_result_display(game, team)
     rs = game&.result_and_scores_for(team)
     return [] if !rs || rs.empty?
