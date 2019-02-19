c284f59e78a221c84a04d73943fc634baabf828b
Naoyuki Kumagai <kumagai@namaka>
2019-02-19 09:30:37 +0900

    'leagues#show' to includes :game when preparing @schedules_by_week

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -6,6 +6,6 @@ class LeaguesController < ApplicationController
 
   def show
     @league = League.find(params[:id])
-    @schedules_by_week = @league.schedules.group_by(&:week)
+    @schedules_by_week = @league.schedules.includes(:game).group_by(&:week)
   end
 end
