3e12e26ba3b5503987fcb73fcd2948e6af159765
Naoyuki Kumagai <kumagai@namaka>
2019-02-20 09:26:29 +0900

    Add value object TeamStanding

---
+++ app/models/team_standing.rb
@@ -0,0 +1,22 @@
+class TeamStanding
+  include Comparable
+
+  attr_reader :team, :won, :lost, :tied
+
+  def initialize(team)
+    @team = team
+    @won, @lost, @tied = team.won_lost_tied
+  end
+
+  def games
+    won + lost + tied
+  end
+
+  def pct
+    -(@won + @tied * 0.5)
+  end
+
+  def <=>(other)
+    pct <=> other.pct
+  end
+end
