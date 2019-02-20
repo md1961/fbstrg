1924499b88aa990989d5ec9615408464836b7876
Naoyuki Kumagai <kumagai@namaka>
2019-02-20 11:20:05 +0900

    Modify league standings to display PF and PA as well

--- app/models/team_standing.rb
+++ app/models/team_standing.rb
@@ -1,11 +1,11 @@
 class TeamStanding
   include Comparable
 
-  attr_reader :team, :won, :lost, :tied
+  attr_reader :team, :won, :lost, :tied, :pf, :pa
 
   def initialize(team)
     @team = team
-    @won, @lost, @tied = team.won_lost_tied_pf_pa
+    @won, @lost, @tied, @pf, @pa = team.won_lost_tied_pf_pa
   end
 
   def games
