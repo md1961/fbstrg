3c1e4c7f39aa4601ba13b6777bf4fdf28cecf1a3
Naoyuki Kumagai <kumagai@namaka>
2019-02-21 14:56:27 +0900

    Alter PlayUtil.write_scorings() for old games before introducing enum :scoring of Play

--- app/services/play_util.rb
+++ app/services/play_util.rb
@@ -4,30 +4,19 @@ module PlayUtil
   def write_scorings(plays)
     (plays.sort_by(&:number) + [nil] * 2).each_cons(3) do |play, next1, next2|
       break unless next1
-      score_curr = play .game_snapshot.total_score
-      score_next = next1.game_snapshot.total_score
-      next unless score_next > score_curr
-      case score_next - score_curr
+      score_curr  = play  .game_snapshot .total_score
+      score_next1 = next1 .game_snapshot .total_score
+      score_next2 = next2&.game_snapshot&.total_score
+      next unless score_next1 > score_curr
+      case score_next1 - score_curr
       when 6
-        no_xp = next2&.game_snapshot&.total_score == score_next ? 'NO ' : ''
-        play.scoring = "TOUCHDOWN (XP #{no_xp}GOOD)"
-        write_score(6 + (no_xp.blank? ? 1 : 0), play)
+        next1.extra_point! if score_next2 && score_next2 > score_next1
+        play.touchdown!
       when 3
-        play.scoring = 'FIELD GOAL'
-        write_score(3, play)
+        play.field_goal!
       when 2
-        play.scoring = 'SAFETY'
-        write_score(2, play)
+        play.safety!
       end
     end
   end
-
-    def write_score(score, play)
-      gss = play.game_snapshot
-      if score >= 3 && !play.possession_changing?
-        gss.home_has_ball ? gss.score_home += score : gss.score_visitors += score
-      else
-        gss.home_has_ball ? gss.score_visitors += score : gss.score_home += score
-      end
-    end
 end
