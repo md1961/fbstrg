3553e7189b5f2a72dc8c242077efbbd2a2039ef4
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2019-02-23 09:42:08 +0900

    Modify Team::Stats to tally kick stats

--- app/models/stats/team.rb
+++ app/models/stats/team.rb
@@ -1,23 +1,26 @@
 module Stats
 
 class Team
-  attr_reader :team, :pass_stats, :run_stats
+  attr_reader :team, :pass_stats, :run_stats, :kick_stats
 
   def initialize(team)
     @team = team
     @pass_stats = Stats::Pass.new(team)
     @run_stats  = Stats::Run.new(team)
+    @kick_stats = Stats::Kick.new(team)
   end
 
   def tally_from(play)
     @pass_stats.tally_from(play)
     @run_stats .tally_from(play)
+    @kick_stats.tally_from(play)
   end
 
   def add(other)
     raise "Teams don't match ('#{team.abbr}' vs '#{other.team.abbr}')" unless team == other.team
     @pass_stats.add(other.pass_stats)
-    @run_stats.add(other.run_stats)
+    @run_stats .add(other.run_stats)
+    @kick_stats.add(other.kick_stats)
   end
 end
 
