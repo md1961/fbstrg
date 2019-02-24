b7cdbce2f45cf2524b565a55baa8a5ee60f1f02a
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2019-02-24 09:47:48 +0900

    Add seeds teams_1962.rb to add sixth team with trait qb_read

---
+++ db/seeds/teams_1962.rb
@@ -0,0 +1,61 @@
+year = 1962
+
+leagues = League.where("year = ?", year)
+unless leagues.size == 1
+  STDERR.puts "Number of leagues for #{year} is #{leagues.size}"
+  exit
+end
+league = leagues.first
+
+teams_existing = league.teams
+{
+  MIA: 5,
+  NYJ: 3,
+  CHI: 0,
+  GB:  4,
+  BAL: 2,
+}.each do |abbr, qb_read|
+  team = teams_existing.find_by(abbr: abbr.to_s)
+  raise "Cannot find Team with abbr of '#{abbr}'" unless team
+  team.team_trait.update!(qb_read: qb_read)
+end
+
+
+%w[KC].each do |abbr|
+  team = league.teams.find_by(abbr: abbr)
+  team.destroy if team
+end
+
+result_chart = PlayResultChart.first
+offensive_strategy = OffensiveStrategy.first
+defensive_strategy = DefensiveStrategy.first
+
+league.teams << \
+  Team.create!(
+    name:               'Kansas City',
+    abbr:               'KC',
+    play_result_chart:  result_chart,
+    offensive_strategy: offensive_strategy,
+    defensive_strategy: defensive_strategy,
+  ).tap { |team|
+    team.create_team_trait!(
+      run_yardage:       2,
+      run_breakaway:     3,
+      pass_short:        5,
+      pass_long:         1,
+      pass_breakaway:    1,
+      pass_protect:      0,
+      qb_mobility:       4,
+      qb_read:           5,
+
+      run_defense:       4,
+      run_tackling:      5,
+      pass_rush:         4,
+      pass_coverage:     4,
+      pass_tackling:     3,
+
+      place_kicking:     4,
+      return_breakaway:  5,
+      return_coverage:   5,
+    )
+  }
