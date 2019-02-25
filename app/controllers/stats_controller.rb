ec28a14f427a0847256289fb975e035e5a8930ff
Naoyuki Kumagai <kumagai@namaka>
2019-02-25 11:00:46 +0900

    Modify 'stats#index' to offer link to 'League'

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -3,5 +3,6 @@ class StatsController < ApplicationController
   def index
     game = Game.find_by(id: params[:game_id]) || Game.first
     @stats = Stats::Game.new(game)
+    @league = game.league
   end
 end
