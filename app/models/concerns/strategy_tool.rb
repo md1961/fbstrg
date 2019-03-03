3a9ee58dcbc872df2011a33f0d9439754ee19be6
Naoyuki Kumagai <kumagai@namaka>
2019-03-04 08:45:50 +0900

    Fix tries_hail_mary?() to use time_left instead of timeout_left

--- app/models/concerns/strategy_tool.rb
+++ app/models/concerns/strategy_tool.rb
@@ -121,8 +121,8 @@ module StrategyTool
   end
 
   def tries_hail_mary?(game)
-    game.quarter == 4 && game.ball_on.between?(40, 60) && game.score_diff < -3 && game.timeout_left < 10 \
-      || game.quarter == 2 && game.ball_on.between?(50, 60) && game.score_diff <= 14 && game.timeout_left < 10
+    (game.quarter == 4 && game.ball_on.between?(40, 60) && game.score_diff < -3 && game.time_left < 10) \
+      || (game.quarter == 2 && game.ball_on.between?(50, 60) && game.score_diff <= 14 && game.time_left < 10)
   end
 
   def needs_offense_timeout?(game)
