dd06a11c1e729299c6ea5810df43c765cbc82723
Naoyuki Kumagai <kumagai@namaka>
2019-03-08 15:04:35 +0900

    Add lib/batch/offensive_play_set_alter_by_crossing_pass.rb

---
+++ lib/batch/offensive_play_set_alter_by_crossing_pass.rb
@@ -0,0 +1,10 @@
+crossing_pass    = OffensivePlay.crossing_pass
+button_hook_pass = OffensivePlay.button_hook_pass
+razzle_dazzle    = OffensivePlay.razzle_dazzle
+
+OffensivePlaySet.all.each do |offensive_play_set|
+  choice = offensive_play_set.offensive_play_set_choices.find_by(offensive_play: crossing_pass)
+  offensive_play_set.offensive_play_set_choices.create!(offensive_play: razzle_dazzle, weight: choice.weight)
+  choice_b = offensive_play_set.offensive_play_set_choices.find_by(offensive_play: button_hook_pass)
+  choice.update!(weight: choice_b.weight)
+end
