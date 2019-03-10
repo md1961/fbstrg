8dfd372e9e762c8af247c5abf97f61ff3e5e9e9c
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2019-03-10 13:54:06 +0900

    Modify 'team_traits#inc,dec' to redirect back using flash

--- app/controllers/team_traits_controller.rb
+++ app/controllers/team_traits_controller.rb
@@ -3,12 +3,12 @@ class TeamTraitsController < ApplicationController
   def decrement
     team_trait = TeamTrait.find(params[:id])
     team_trait.decrement!(params[:name])
-    redirect_to teams_path(team_trait_id: team_trait)
+    redirect_to teams_path, flash: {team_trait_id: team_trait.id}
   end
 
   def increment
     team_trait = TeamTrait.find(params[:id])
-    TeamTrait.find(params[:id]).increment!(params[:name])
-    redirect_to teams_path(team_trait_id: team_trait)
+    team_trait.increment!(params[:name])
+    redirect_to teams_path, flash: {team_trait_id: team_trait.id}
   end
 end
