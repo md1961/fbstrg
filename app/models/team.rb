8aad1e37229ab2616a43d3373fcca96cb6ee78e1
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2019-03-10 09:13:48 +0900

    Modify Team to set strategies and team_traits on create

--- app/models/team.rb
+++ app/models/team.rb
@@ -5,8 +5,13 @@ class Team < ApplicationRecord
   belongs_to :defensive_strategy
   has_one :team_trait, dependent: :destroy
 
+  before_validation :set_strategies_and_traits, on: :create
+
+  validates :name, presence: true
+  validates :abbr, presence: true
+
   def human_assisted?
-    name == 'Miami' || %w[H V].include?(abbr)
+    name == 'Miami' || %w[H].include?(abbr)
   end
 
   def league
@@ -25,4 +30,13 @@ class Team < ApplicationRecord
   def to_s
     name
   end
+
+  private
+
+    def set_strategies_and_traits
+      self.play_result_chart_id  = 2
+      self.offensive_strategy_id = 1
+      self.defensive_strategy_id = 1
+      self.team_trait = TeamTrait.new
+    end
 end
