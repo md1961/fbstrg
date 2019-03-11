6373969e5fa127ef53fc30da709335ec8df28bc3
Naoyuki Kumagai <kumagai@namaka>
2019-03-11 10:28:36 +0900

    Fix League.create_next() to save TeamTrait's as well

--- app/models/league.rb
+++ app/models/league.rb
@@ -4,15 +4,17 @@ class League < TeamGroup
 
   def self.create_next
     last_league = League.order(:updated_at).last
-    create!(
-      name: last_league.name,
-      year: last_league.year + 1,
-      direct_teams: last_league.teams.map { |team|
-        team.dup.tap { |new_team|
-          new_team.team_trait = team.team_trait.dup
+    ApplicationRecord.transaction do
+      create!(
+        name: last_league.name,
+        year: last_league.year + 1,
+        direct_teams: last_league.teams.map { |team|
+          team.dup.tap { |new_team|
+            new_team.update!(team_trait: team.team_trait.dup)
+          }
         }
-      }
-    )
+      )
+    end
   end
 
   def prev_league
