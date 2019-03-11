0da4da0bf91376dd12459cac7048e93085634610
Naoyuki Kumagai <kumagai@namaka>
2019-03-11 10:59:36 +0900

    Add TeamGroup#make_round_robin_schedules() replacing League#make_schedules()

--- app/models/league.rb
+++ app/models/league.rb
@@ -53,31 +53,6 @@ class League < TeamGroup
     schedules.detect { |schedule| !schedule.game.final? }
   end
 
-  def make_schedules
-    return if teams.empty?
-    team_ids = teams.map(&:id).sort_by { rand }
-    team_surplus = team_ids.size.odd? ? nil : Team.find(team_ids.pop)
-    team_ids.size.times do |week|
-      (team_ids.size / 2 + 1).times do |i|
-        h = Team.find(team_ids[i])
-        v = Team.find(team_ids[-(i + 1)])
-        if h == v
-          if !team_surplus
-            next
-          else
-            v = team_surplus
-          end
-        end
-        h, v = v, h if week.even?
-        game = Game.new(home_team: h, visitors: v)
-        schedules.build(week: week + 1, number: i + 1, game: game)
-        game = Game.new(home_team: v, visitors: h)
-        schedules.build(week: week + 1 + team_ids.size, number: i + 1, game: game)
-      end
-      team_ids.unshift(team_ids.pop)
-    end
-  end
-
   def to_s
     "#{year} #{abbr}"
   end
--- app/models/team_group.rb
+++ app/models/team_group.rb
@@ -10,6 +10,31 @@ class TeamGroup < ApplicationRecord
     child_groups.flat_map(&:teams)
   end
 
+  def make_round_robin_schedules
+    return if teams.empty?
+    team_ids = teams.map(&:id).sort_by { rand }
+    team_surplus = team_ids.size.odd? ? nil : Team.find(team_ids.pop)
+    team_ids.size.times.each_with_object([]) { |week, schedules|
+      (team_ids.size / 2 + 1).times do |i|
+        h = Team.find(team_ids[i])
+        v = Team.find(team_ids[-(i + 1)])
+        if h == v
+          if !team_surplus
+            next
+          else
+            v = team_surplus
+          end
+        end
+        h, v = v, h if week.even?
+        game = Game.new(home_team: h, visitors: v)
+        schedules << Schedule.new(week: week + 1, number: i + 1, game: game)
+        game = Game.new(home_team: v, visitors: h)
+        schedules << Schedule.new(week: week + 1 + team_ids.size, number: i + 1, game: game)
+      end
+      team_ids.unshift(team_ids.pop)
+    }
+  end
+
   private
 
     def set_abbr
