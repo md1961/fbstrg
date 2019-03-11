106369ec9c829ace0fc821b71d717567382f7d7f
Naoyuki Kumagai <kumagai@namaka>
2019-03-11 09:31:25 +0900

    Fix TeamGroup belongs_to :parent by specifying class_name

--- app/models/team_group.rb
+++ app/models/team_group.rb
@@ -1,4 +1,4 @@
 class TeamGroup < ApplicationRecord
   has_many :direct_teams, class_name: 'Team'
-  belongs_to :parent, foreign_key: 'parent_id', optional: true
+  belongs_to :parent, class_name: 'TeamGroup', foreign_key: 'parent_id', optional: true
 end
