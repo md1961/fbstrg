4df3128b4b6018be739f91692774c37e0dbff89e
Naoyuki Kumagai <kumagai@namaka>
2019-03-15 10:52:38 +0900

    DB backup (1963 week 4-3)

--- app/services/clocker.rb
+++ app/services/clocker.rb
@@ -6,11 +6,12 @@ module Clocker
     game.clock_stopped = play.incomplete? || play.intercepted? || play.fumble_rec_by_opponent? \
                       || play.field_goal_try? || play.kick_and_return? || play.onside_kick? \
                       || play.penalty? || !play.no_scoring? || play.possession_changed? \
-                      || (play.out_of_bounds? && clock_stops_when_out_of_bounds?(game))
+                      || (play.out_of_bounds? && clock_stops_when_out_of_bounds?(game, play))
     play.time_to_take
   end
 
-    def clock_stops_when_out_of_bounds?(game)
-      (game.quarter == 2 && game.time_left <= 2 * 60) || (game.quarter >= 4 && game.time_left <= 5 * 60)
+    def clock_stops_when_out_of_bounds?(game, play)
+      time_left = game.time_left - play.time_to_take
+      (game.quarter == 2 && time_left <= 2 * 60) || (game.quarter >= 4 && time_left <= 5 * 60)
     end
 end
