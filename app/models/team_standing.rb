8b7b1a9445376f0614c58eb07f8b19853469a7b0
Naoyuki Kumagai <kumagai@namaka>
2019-04-04 15:02:50 +0900

    Modify TeamStanding to hold record within conference

--- app/models/team_standing.rb
+++ app/models/team_standing.rb
@@ -1,11 +1,13 @@
 class TeamStanding
   include Comparable
 
-  attr_reader :team, :won, :lost, :tied, :pf, :pa
+  attr_reader :team, :conference
+  delegate :won, :lost, :tied, :pf, :pa, to: :@league
 
   def initialize(team)
     @team = team
-    @won, @lost, @tied, @pf, @pa = team.won_lost_tied_pf_pa
+    @league     = Record.new(team.won_lost_tied_pf_pa)
+    @conference = Record.new(team.won_lost_tied_pf_pa(within: :conference))
   end
 
   def games
@@ -13,10 +15,22 @@ class TeamStanding
   end
 
   def pct
-    -(@won + @tied * 0.5)
+    -(won + tied * 0.5)
   end
 
   def <=>(other)
     pct <=> other.pct
+  end
+
+  class Record
+    attr_reader :won, :lost, :tied, :pf, :pa
+
+    def initialize(args)
+      @won, @lost, @tied, @pf, @pa = args
+    end
+
+    def to_s
+      [won, lost, tied].join('-')
+    end
   end
 end
