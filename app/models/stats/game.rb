4b380096ae125540fffd7d65add2c15cd6dc2540
Naoyuki Kumagai <kumagai@namaka>
2019-04-09 09:08:16 +0900

    Modify Stats::Team to handle kickoff, punt and interception return stats

--- app/models/stats/game.rb
+++ app/models/stats/game.rb
@@ -15,8 +15,13 @@ class Game
     def tally
       plays = @game.plays.includes(:game_snapshot)
       plays.each do |play|
-        stat = play.game_snapshot.home_has_ball ? @stats_home : @stats_visitors
-        stat.tally_from(play)
+        stat_offense, stat_defense = if play.game_snapshot.home_has_ball
+                                       [@stats_home, @stats_visitors]
+                                     else
+                                       [@stats_visitors, @stats_home]
+                                     end
+        stat_offense.tally_offense_from(play)
+        stat_defense.tally_return_from(play)
       end
 
       @stats_home    .set_defense_stats(@stats_visitors)
--- app/models/stats/team.rb
+++ app/models/stats/team.rb
@@ -1,7 +1,8 @@
 module Stats
 
 class Team
-  attr_reader :team, :pass_stats, :run_stats, :kick_stats, :pass_defense_stats, :run_defense_stats
+  attr_reader :team, :pass_stats, :run_stats, :kick_stats, :pass_defense_stats, :run_defense_stats,
+              :kickoff_return_stats, :punt_return_stats, :int_return_stats
 
   def initialize(team)
     @team = team
@@ -10,12 +11,21 @@ class Team
     @kick_stats = Stats::Kick.new(team)
     @pass_defense_stats = Stats::Pass.new(team)
     @run_defense_stats  = Stats::Run.new(team)
+    @kickoff_return_stats = Stats::KickoffReturn.new(team)
+    @punt_return_stats    = Stats::PuntReturn.new(team)
+    @int_return_stats     = Stats::IntReturn.new(team)
   end
 
-  def tally_from(play)
+  def tally_offense_from(play)
     @pass_stats.tally_from(play)
     @run_stats .tally_from(play)
     @kick_stats.tally_from(play)
+  end
+
+  def tally_return_from(play)
+    @kickoff_return_stats.tally_from(play)
+    @punt_return_stats   .tally_from(play)
+    @int_return_stats    .tally_from(play)
   end
 
   def set_defense_stats(opponent_stats)
@@ -30,6 +40,9 @@ class Team
     @kick_stats.add(other.kick_stats)
     @pass_defense_stats.add(other.pass_defense_stats)
     @run_defense_stats .add(other.run_defense_stats)
+    @kickoff_return_stats.add(other.kickoff_return_stats)
+    @punt_return_stats   .add(other.punt_return_stats)
+    @int_return_stats    .add(other.int_return_stats)
   end
 end
 
