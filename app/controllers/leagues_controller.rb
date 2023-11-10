09181a864bc561e1b700ec15db393f6e43a03c78
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2023-11-10 14:01:00 +0900

    Revert "Modify 'leagues#show' for options to toggle pretty display"
    This reverts commit af096d53d75454cba86f7395db2a4f0e1e3bc720.

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -9,7 +9,5 @@ class LeaguesController < ApplicationController
     @schedules_by_week = @league.schedules.includes(:game).group_by(&:week)
     @game_played_last = @league.games_finished.sort_by(&:updated_at).last
     @shows_stats = params[:shows_stats] == 'true'
-
-    session[:pretty_display] = params[:pretty] == 'true' if params[:pretty]
   end
 end
