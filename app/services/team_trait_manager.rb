05c63b017b1eceb26ef5192c0e45abdb7258f295
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2023-11-17 19:27:11 +0900

    Alter TeamTraitManager to apply home field advange only on defense

--- app/services/team_trait_manager.rb
+++ app/services/team_trait_manager.rb
@@ -9,60 +9,58 @@ class TeamTraitManager
   end
 
   def place_kicking_factor
-    @offense_trait.place_kicking + home_factor
+    @offense_trait.place_kicking
   end
 
   def pass_protect_factor
-    @offense_trait.pass_protect - @defense_trait.pass_rush + home_factor
+    @offense_trait.pass_protect - @defense_trait.pass_rush - home_defense_factor
   end
 
   def run_breakaway_factor
-    @offense_trait.run_breakaway - @defense_trait.run_tackling + home_factor
+    @offense_trait.run_breakaway - @defense_trait.run_tackling - home_defense_factor
   end
 
   def pass_breakaway_factor
-    @offense_trait.pass_breakaway - @defense_trait.pass_tackling + home_factor
+    @offense_trait.pass_breakaway - @defense_trait.pass_tackling - home_defense_factor
   end
 
   def return_breakaway_factor
-    @defense_trait.return_breakaway - @offense_trait.return_coverage + home_factor(false)
+    @defense_trait.return_breakaway - @offense_trait.return_coverage
   end
 
   def run_yardage_factor
-    # (1 .. 20) + home_factor
-    @offense_trait.run_yardage - @defense_trait.run_defense + 10 + home_factor
+    # (1 .. 20) - home_defense_factor
+    @offense_trait.run_yardage - @defense_trait.run_defense + 10 - home_defense_factor
   end
 
   # TODO: Consider to reduce pass_complete_factor.
   def pass_complete_factor
     multiplier = @offensive_play.short_pass? ? 5.0 : @offensive_play.medium_pass? ? 4.0 : 3.0
-    (pass_offense_factor - @defense_trait.pass_coverage + home_factor) * multiplier
+    (pass_offense_factor - @defense_trait.pass_coverage - home_defense_factor) * multiplier
   end
 
   def pass_yardage_factor
     defense_factor = rand(2).zero? ? @defense_trait.pass_coverage : @defense_trait.pass_tackling
-    pass_offense_factor - defense_factor + home_factor
+    pass_offense_factor - defense_factor - home_defense_factor
   end
 
   def pass_interception_factor
     # -10 .. 10 => 0.0 .. 2.0
-    f = pass_offense_factor - @defense_trait.pass_coverage + home_factor
+    f = pass_offense_factor - @defense_trait.pass_coverage - home_defense_factor
     [f * -0.1 + 1.0, 0.1].max
   end
 
   def qb_read_factor
-    @offense_trait.qb_read + home_factor
+    @offense_trait.qb_read - home_defense_factor
   end
 
   private
 
     # Home field advantage is in effect in (the ratio below to 1).
-    HOME_FIELD_ADVANTAGE_RATIO = 10
+    HOME_FIELD_ADVANTAGE_RATIO = 8
 
-    def home_factor(for_offense = true)
-      return 0 if @is_neutral_game
-      return 0 if  @home_has_ball && !for_offense
-      return 0 if !@home_has_ball &&  for_offense
+    def home_defense_factor
+      return 0 if @is_neutral_game || @home_has_ball
       rand(HOME_FIELD_ADVANTAGE_RATIO).zero? ? 1 : 0
     end
 
