af5162cc6dd5d0113e33190e2a2efc2c912f2c4a
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2023-11-17 19:14:51 +0900

    Modify StrategyTool#needs_defense_timeout?() to depend on scores

--- app/models/concerns/strategy_tool.rb
+++ app/models/concerns/strategy_tool.rb
@@ -167,10 +167,10 @@ module StrategyTool
 
   def needs_defense_timeout?(game)
     return false if game.clock_stopped || game.timeout_left(false) <= 0
-    return false if [1, 3].include?(game.quarter) || game.time_left >= 60 * 3
-    game.quarter == 4 && game.score_diff > 0 && (
-      # TODO: Adjust conditions depending on scores.
-      true
+    return false if [1, 3].include?(game.quarter) || game.time_left >= 60 * 4
+    game.quarter == 4 && (
+         (game.score_diff > 8 && game.time_left <= 60 * 4) \
+      || (game.score_diff > 0 && game.time_left <= 60 * 3)
     ) ||
     game.quarter == 2 && (
          (game.down == 4 && !tries_fourth_down_gamble?(game)) \
