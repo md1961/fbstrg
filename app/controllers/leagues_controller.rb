34bac5c9ac4189816159e0ad30c59c91af66a711
Naoyuki Kumagai <kumagai@namaka>
2023-11-21 11:38:20 +0900

    Modify 'leagues#show' to display playoff schedules

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -7,6 +7,7 @@ class LeaguesController < ApplicationController
   def show
     @league = League.find(params[:id])
     @schedules_by_week = @league.schedules.includes(:game).regulars.group_by(&:week)
+    @playoffs_by_week  = @league.schedules.includes(:game).playoffs.group_by(&:week)
     @game_played_last = @league.games_finished.sort_by(&:updated_at).last
     @shows_stats = params[:shows_stats] == 'true'
   end
