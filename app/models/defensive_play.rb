6778e425739386552960df7884ad5a8126a6f2f4
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2023-12-03 07:55:26 +0900

    Add DefensivePlay.pick_from()

--- app/models/defensive_play.rb
+++ app/models/defensive_play.rb
@@ -1,6 +1,18 @@
 class DefensivePlay < ApplicationRecord
   has_many :play_results
 
+  def self.pick_from(names)
+    return nil if names.blank?
+
+    unless valid_combination?(names)
+      raise Exceptions::IllegalResultStringError, "Illegal defensive play '#{names}'"
+    end
+
+    names.upcase.split(//).sample.then { |name|
+      DefensivePlay.find_by(name: name)
+    }
+  end
+
   def formation
     [lineman, linebacker, cornerback, safety].join('-')
   end
@@ -25,4 +37,18 @@ class DefensivePlay < ApplicationRecord
     "#{name}: #{formation}\n" \
       + "Run: #{against_run}, Pass: #{against_pass}"
   end
+
+  private
+
+    def self.valid_combination?(names)
+      names.upcase =~ /\A[A-J]+\z/ \
+        && \
+      names.upcase.delete('CH').split(//).uniq.map { |c|
+        'ABDEFGIJ'.index(c)
+      }.minmax.then { |min, max|
+        max && min ? max - min : 0
+      } <= 3 \
+        && \
+      names.upcase.delete('ABDEFG').split(//).uniq.sort.join !~ /\A[CH]+[IJ]+\z/
+    end
 end
--- test/models/defensive_play_test.rb
+++ test/models/defensive_play_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class DefensivePlayTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test ".pick_from()" do
+    %w[a j abcd fgij ac ah gc gh].each do |names|
+      assert_nothing_raised do
+        DefensivePlay.pick_from(names)
+      end
+    end
+
+    %w[k abdef efgij ci cj hi hj].each do |names|
+      assert_raises(Exceptions::IllegalResultStringError) do
+        DefensivePlay.pick_from(names)
+      end
+    end
+  end
 end
