352ad0f6daa9b7d376e1d459bf5bc2a8c5e15d49
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2023-12-12 10:14:29 +0900

    Modify 'teams#index' to emphasize TeamTrait value changes

--- app/helpers/teams_helper.rb
+++ app/helpers/teams_helper.rb
@@ -1,7 +1,15 @@
 module TeamsHelper
 
-  def team_trait_display(team_trait, trait_name)
+  def team_trait_display(team_trait, trait_name, value_last = nil)
     return nil unless team_trait
-    team_trait.send(trait_name).then { |v| v > 0 ? "+#{v}" : v }
+    value = team_trait.send(trait_name).then { |v| v > 0 ? "+#{v}" : v }
+    if value_last.nil?
+      value
+    else
+      clazz = [nil, :increased, :decreased][value.to_i <=> value_last.to_i]
+      content_tag :span, class: clazz do
+        value.to_s
+      end
+    end
   end
 end
