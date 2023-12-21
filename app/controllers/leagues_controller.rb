75924f6c13091e69815aea8680284938769c48ed
Naoyuki Kumagai <kumagai@namaka>
2023-12-21 11:16:26 +0900

    Modify 'leagues#show' to optionally display schedules in current week

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -6,9 +6,18 @@ class LeaguesController < ApplicationController
 
   def show
     @league = League.find(params[:id])
+
     @schedules_by_week = @league.schedules.includes(:game).regulars.group_by(&:week)
     @playoffs_by_week  = @league.schedules.includes(:game).playoffs.group_by(&:week)
     @game_played_last = @league.games_finished.sort_by(&:updated_at).last
+
+    @displays_current_week = if params[:schedule_display]
+                               params[:schedule_display] == 'current_week'
+                             else
+                               session[:displays_current_week]
+                             end
+    session[:displays_current_week] = @displays_current_week
+
     @shows_stats = params[:shows_stats] == 'true'
   end
 end
