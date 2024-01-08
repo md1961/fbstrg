58306cbeaf578fcb17b66f135d5461ca99743b35
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2024-01-08 10:19:44 +0900

    Modify 'teams#index' for colors depending on values and changes

--- app/helpers/teams_helper.rb
+++ app/helpers/teams_helper.rb
@@ -2,14 +2,15 @@ module TeamsHelper
 
   def team_trait_display(team_trait, trait_name, value_last = nil)
     return nil unless team_trait
+
     value = team_trait.send(trait_name).then { |v| v > 0 ? "+#{v}" : v }
-    if value_last.nil?
-      value
-    else
-      clazz = [nil, :increased, :decreased][value.to_i <=> value_last.to_i]
-      content_tag :span, class: clazz do
-        value.to_s
-      end
+    classes = [[nil, :positive, :negative][value.to_i <=> 0]]
+    if value_last
+      classes << [nil, :increased, :decreased][value.to_i <=> value_last.to_i]
+    end
+
+    content_tag :span, class: classes.compact.join(' ') do
+      value.to_s
     end
   end
 
