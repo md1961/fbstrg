5dd33bc12dd4920081d22e2d766dc43f309831f1
Naoyuki Kumagai <kumagai@namaka>
2024-07-09 14:25:23 +0900

    Add resources route :offensive_play_sets and :defensive_play_sets

--- config/routes.rb
+++ config/routes.rb
@@ -30,5 +30,8 @@ Rails.application.routes.draw do
   resources :play_sets, only: :index
   resources :off_def_charts, only: :index
 
+  resources :offensive_play_sets, only: %i[create update]
+  resources :defensive_play_sets, only: %i[create update]
+
   resources :miscs, only: :index
 end
