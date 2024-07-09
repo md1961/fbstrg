cfb8580d442acee0119ba1e35f89b92cb0792c35
Naoyuki Kumagai <kumagai@namaka>
2024-07-09 14:27:55 +0900

    Modify 'play_sets#index' to prepare @play_set_for_form according to params

--- app/controllers/play_sets_controller.rb
+++ app/controllers/play_sets_controller.rb
@@ -5,5 +5,8 @@ class PlaySetsController < ApplicationController
     @offensive_plays = OffensivePlay.normal_plays
     @defensive_play_sets = DefensivePlaySet.normal.includes(:defensive_play_set_choices)
     @defensive_plays = DefensivePlay.all
+
+    play_set_class = params[:class]&.constantize
+    @play_set_for_form = play_set_class&.find(params[:id])
   end
 end
